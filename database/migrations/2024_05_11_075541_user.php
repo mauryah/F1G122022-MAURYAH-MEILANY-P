5d8343b723eea4935a415bfdbbfbe426b5a7b2cc
mauryah <mauryahmeilany@gmail.com>
2024-05-12 19:53:59 +0800

    first commit

--- database/migrations/2024_05_11_075541_user.php
+++ database/migrations/2024_05_11_075541_user.php
@@ -11,22 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('Username');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -42,8 +31,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('user');
         Schema::dropIfExists('sessions');
     }
 };
